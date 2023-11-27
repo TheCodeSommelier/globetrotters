d665b12436bea87e601848f85a61e41ddf76a5ad
Tony Masek <tm@tony-masek.com>
2023-11-27 17:01:01 +0000

    Create the associations for models

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,8 @@
 class Experience < ApplicationRecord
   belongs_to :journey
+
+  has_many :saved_experiences, dependent: :destroy
+
+  # Only if we want to show how many times has an experince been saved inside of a journeys
+  # has_many :journeys, through: :saved_experiences, dependent: :destroy
 end
