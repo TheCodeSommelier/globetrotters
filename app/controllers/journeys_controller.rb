69986306972078063c4e8f102b9a4cc6199205ac
Howard Symes Thompson <howard.symes.thompson@gmail.com>
2023-11-28 13:01:59 +0000

    user journey for creating a new journey works

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -3,4 +3,19 @@ class JourneysController < ApplicationController
     @journey = Journey.new
   end
 
+  def create
+    @journey = Journey.new(journey_params)
+    @journey.user = current_user
+    if @journey.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def journey_params
+    params.require(:journey).permit(:location, :start_date, :end_date, :category, :notes, :user)
+  end
 end
---
+++ app/javascript/controllers/datepicker_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+// Connects to data-controller="datepicker"
+export default class extends Controller {
+  connect() {
+    flatpickr(this.element)
+    flatpickr(this.element, {
+      // mode: "range",
+      altInput: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d",
+    })
+  }
+}
