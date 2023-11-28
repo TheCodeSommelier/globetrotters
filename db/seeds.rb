cdc462e716a4e1caa2fa7995df36bde7b7fef4e6
Sayyab Khan <sayyabk05@gmail.com>
2023-11-28 11:06:13 +0000

    Created a simple seed file with user profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,46 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Cleaning DB 🧼'
+User.destroy_all
+Experience.destroy_all
+Journey.destroy_all
+SavedExperience.destroy_all
+
+puts 'Creating users xox'
+
+user_details = [
+  {
+    email: 'sayyab@gmail.com',
+    password: "123456",
+    first_name: "Sayyab",
+    last_name: "Khan",
+    username: "s777yab"
+  },
+  {
+    email: 'tony@gmail.com',
+    password: "123456",
+    first_name: "Tony",
+    last_name: "Masek",
+    username: "tonyM"
+  },
+  {
+    email: 'marco@gmail.com',
+    password: "123456",
+    first_name: "Marco",
+    last_name: "Di Leo",
+    username: "marcoDL"
+  },
+  {
+    email: 'howard@gmail.com',
+    password: '123456',
+    first_name: "Howard",
+    last_name: "Thompson",
+    username: "howardT"
+  }
+]
+
+user_details.map do |details|
+  User.create(details)
+end
