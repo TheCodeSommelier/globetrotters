e16730e2daf9d834d83769eea7e2bb9ad849a821
Sayyab Khan <sayyabk05@gmail.com>
2023-11-29 11:46:14 +0000

    added a list of journeys on user page without css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def profile_page
     @user = current_user
+    @journeys = @user.journeys
+    # @experiences = @user.experiences
   end
 end
+
+# Could get all instances of experiences as an array and iterate and show on page
