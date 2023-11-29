f8a4dd0b3a81a9c6f2a8e400c9c4ad5443391e06
Sayyab Khan <sayyabk05@gmail.com>
2023-11-29 12:42:55 +0000

    list shows and images are in process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,5 @@ class UsersController < ApplicationController
   def profile_page
     @user = current_user
     @journeys = @user.journeys
-    # @experiences = @user.experiences
   end
 end
-
-# Could get all instances of experiences as an array and iterate and show on page
