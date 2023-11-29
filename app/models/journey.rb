e17535ced50d616c7c8d7d4d828cc9ae133a02c8
Howard Symes Thompson <howard.symes.thompson@gmail.com>
2023-11-29 15:34:59 +0000

    added user attributes to sign up form and should show avatar

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -15,20 +15,20 @@ class Journey < ApplicationRecord
       }
 
   # Before seeding comment from line out till line 25 and once the seed is done put this back in
-  after_create :prebuild_sightseeing_list
+  # after_create :prebuild_sightseeing_list
 
-  private
+  # private
 
-  def prebuild_sightseeing_list
-    location = self.location
-    # Use with geocoder
-    # experience.address.near(self.location)
-    experience_in_location = Experience.all.select do |experience|
-      experience.journey.location == location
-    end
+  # def prebuild_sightseeing_list
+  #   location = self.location
+  #   # Use with geocoder
+  #   # experience.address.near(self.location)
+  #   experience_in_location = Experience.all.select do |experience|
+  #     experience.journey.location == location
+  #   end
 
-    experiences_top_most_liked = experience_in_location.sort_by(&:likes).reverse.first(5)
+  #   experiences_top_most_liked = experience_in_location.sort_by(&:likes).reverse.first(5)
 
-    experiences_top_most_liked.each { |experience| SavedExperience.create!(experience: experience, journey: self) }
-  end
+  #   experiences_top_most_liked.each { |experience| SavedExperience.create!(experience: experience, journey: self) }
+  # end
 end
