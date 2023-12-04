55840df1bfeb828987210f315a1331cb4644a646
Sayyab Khan <sayyabk05@gmail.com>
2023-12-04 09:33:58 +0000

    basic chatroom works but still need to make it private

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,13 +1,14 @@
 class ChatroomsController < ApplicationController
   def index
-    @chatrooms = Chatroom.all
+    @chatrooms = Chatroom.where(id: current_user)
   end
 
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
-  def create
 
+  def create
+    # Create a button that makes a POST request which creates a new chatroom.
   end
 end
