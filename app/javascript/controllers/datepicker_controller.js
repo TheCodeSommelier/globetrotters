8b47ba38c10baa35b6621aac989f0ca4f712e1a9
Howard Symes Thompson <howard.symes.thompson@gmail.com>
2023-12-06 16:13:03 +0000

    flatpickr selected dates change colour but only after reclick

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -7,16 +7,18 @@ export default class extends Controller {
   static targets= ["dates", "start", "end"]
 
   connect() {
-    console.log(this.datesTarget)
-    console.log(this.startTarget)
-    console.log(this.endTarget)
 
     flatpickr(this.datesTarget, {
       mode: "range",
+
       onChange: ([start, end]) => {
         if (start && end) {
           this.startTarget.value = start
           this.endTarget.value = end
+          const startIcon = document.getElementsByClassName("selected")
+          startIcon[0].classList.add("flatpickr-edit")
+          const endIcon = document.getElementsByClassName("selected")
+          endIcon[1].classList.add("flatpickr-edit")
         }
       },
       dateFormat: "d-m-Y",
